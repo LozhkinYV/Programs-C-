31909d2eeb7b16af0092c8f817b64f87e86a77c1
unknown <user@tau-n-44.tau.local>
2022-09-30 11:24:31 +0400

    Example005_If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша") // .ToLower() позволяет вкл нижний регистр, т.е. мыможем хоть строчные хоть заглавные в имени писать - это все одно будет
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
