0e1d8d7583303560eff02e984dc72f3db7db9a8b
unknown <user@tau-n-44.tau.local>
2022-10-17 15:55:25 +0400

    Example013_2Array_Rekursion

---
+++ Example013_2Array_Rekursion/Program.cs
@@ -0,0 +1,31 @@
+﻿//двумерные массивы
+
+void PrintArray (int[,] matr) //создаем двумерный массив
+{
+for (int i = 0; i < matr.GetLength(0); i++) //GetLength(0) это длина строки
+{
+    for (int j =0; j < matr.GetLength(1); j++) //GetLength(1) это длина столбца
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray (int[,] matr) // заполняем рандомными числами двумерный массив
+{
+    for (int i = 0; i < matr.GetLength(0); i++) //GetLength(0) это длина строки
+{
+    for (int j =0; j < matr.GetLength(1); j++) //GetLength(1) это длина столбца
+    {
+        matr[i,j] = new Random().Next(1,10);
+    }
+
+}   
+}
+
+int [,] matrix = new int [3,4]; // 1-я цифра количество строкб 2-я столбцов (от 0)
+PrintArray (matrix);
+
+FillArray (matrix);
+PrintArray (matrix);
